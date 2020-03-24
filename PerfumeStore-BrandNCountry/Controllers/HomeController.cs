dc75526c6d1f37b34fa8bf3a88a334b906047373
hieuthanh19 <kieuhieuthanh2@gmail.com>
2020-03-25 00:46:24 +0700

    Add Home & store pages

--- PerfumeStore-BrandNCountry/Controllers/HomeController.cs
+++ PerfumeStore-BrandNCountry/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Model.EF;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,16 +10,31 @@ namespace PerfumeStore_BrandNCountry.Controllers
 {
     public class HomeController : Controller
     {
+        private PerfumeStoreDbContext db = new PerfumeStoreDbContext();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Store(int categoryId)
         {
-            ViewBag.Message = "Your application description page.";
+            var products = db.products.Include(p => p.brand).Include(p => p.category);
+            var finalList = products;
+            if (categoryId != -1)
+            {
+                finalList = products.Where(p => p.category_id == categoryId && p.product_status != 0);
+            }
+            else
+                finalList = products.Where(p => p.product_status != 0);
 
-            return View();
+            return View(finalList.ToList());
+        }
+
+        [ChildActionOnly]
+        public ActionResult CategoryList()
+        {
+            return View(db.categories.ToList());
         }
 
         public ActionResult Contact()
